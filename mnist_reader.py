b7b6fe015a02835d5a1f2122ff8cd0898351f5ca
jeffchy <jiangchy@shanghaitech.edu.cn>
2018-12-02 13:52:57 +0800

    have to use 2 label class. finish First order method finish second order method?

--- mnist_reader.py
+++ mnist_reader.py
@@ -20,3 +20,33 @@ def load_mnist(path, kind='train'):
                                offset=16).reshape(len(labels), 784)
 
     return images, labels
+
+def load_mnist_1(path, include_labels, kind='train'):
+    """Load MNIST data from `path`"""
+    import os
+    import gzip
+    import numpy as np
+
+    labels_path = os.path.join(path,
+                               '%s-labels-idx1-ubyte.gz'
+                               % kind)
+    images_path = os.path.join(path,
+                               '%s-images-idx3-ubyte.gz'
+                               % kind)
+
+    with gzip.open(labels_path, 'rb') as lbpath:
+        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
+                               offset=8)
+
+    with gzip.open(images_path, 'rb') as imgpath:
+        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
+                               offset=16).reshape(len(labels), 784)
+
+    if len(include_labels) != 0:
+        label_normalize = dict(zip(include_labels, range(len(include_labels))))
+        labels_mask = np.in1d(labels, include_labels)
+
+        labels = np.array([label_normalize[label] for label in labels[labels_mask]])
+        images = images[labels_mask].reshape(len(labels), 784)
+
+    return images, labels
